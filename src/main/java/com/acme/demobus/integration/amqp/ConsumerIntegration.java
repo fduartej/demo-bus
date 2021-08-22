879ed29c5afc353530a8b6d1330a867218e16cca
Fred <fduartej@gmail.com>
2021-08-22 01:17:23 -0500

    logging

--- src/main/java/com/acme/demobus/integration/amqp/ConsumerIntegration.java
+++ src/main/java/com/acme/demobus/integration/amqp/ConsumerIntegration.java
@@ -1,16 +1,18 @@
 package com.acme.demobus.integration.amqp;
 
 import org.springframework.stereotype.Service;
+import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 
 @Service
 public class ConsumerIntegration<T> {
 
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerIntegration.class);
+
     @RabbitListener(queues = "${demobus.rabbitmq.queue}")
     public void receiveMessage(T o) {
-      System.out.println("Received Message:" + o);
-      System.out.println();
+      logger.debug("Received Message: {} ", o);
     }
     
 }
